877b20ec41e0452ac62607c5c8ba3b820ee27816
Kaio Vitor Santos Queiroz <123708201+Kaio-0708@users.noreply.github.com>
2024-07-05 19:49:26 -0300

    Update AutorService.java

--- literatura/src/main/java/com/alura/literatura/service/AutorService.java
+++ literatura/src/main/java/com/alura/literatura/service/AutorService.java
@@ -1,13 +1,11 @@
 package com.alura.literatura.service;
 
 import com.alura.literatura.model.Autor;
-
 import java.util.List;
 
 public interface AutorService {
 
     List<Autor> listarTodosAutores();
-
     List<Autor> findAutoresVivosPorAno(int ano);
 
 }
